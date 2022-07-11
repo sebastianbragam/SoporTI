182ddc5a2ea5de7ed0c046abba667234985e6ee9
Sebaztianin <seba_braga01@hotmail.com>
2022-07-11 00:27:56 -0300

    2022.07.11 Progress

--- app/src/main/java/com/saltapor/soporti/RegisterActivity.java
+++ app/src/main/java/com/saltapor/soporti/RegisterActivity.java
@@ -3,12 +3,68 @@ package com.saltapor.soporti;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Initialize FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            finish();
+            return;
+        }
+
+        Button btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
+
+        TextView tvLogin = findViewById(R.id.tvLogin);
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToLogin();
+            }
+        });
+
     }
+
+    private void registerUser() {
+        EditText etFirstName = findViewById(R.id.etFirstName);
+        EditText etLastName = findViewById(R.id.etLastName);
+        EditText etRegisterEmail = findViewById(R.id.etRegisterEmail);
+        EditText etRegisterPassword = findViewById(R.id.etRegisterPassword);
+
+        String firstName = etFirstName.getText().toString();
+        String lastName = etLastName.getText().toString();
+        String email = etRegisterEmail.getText().toString();
+        String password = etRegisterPassword.getText().toString();
+
+        if (firstName.isEmpty() || lastName.isEmpty() ||email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Por favor rellene todos los campos", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+    }
+
+    private void switchToLogin() {
+
+    }
+
 }
